caa2968031343ba0051e1f5b454b39503dd913ca
Marlon Klagenberg <marlon.klc@gmail.com>
2022-04-07 02:16:47 -0300

    refactoring to better code ; add animation on finish game

--- anna/jogo-da-memoria/game.js
+++ anna/jogo-da-memoria/game.js
@@ -1,119 +1,155 @@
 const memoryGame = {
     containerElement: null,
-    coverCard: 'pokeball.png',
-    firstCardFlipped: null,
-    secondCardFlipped: null,
-    timeToFlipUnmatchedCards: 1 * 1000, // 1 second
-    waitingFlipUnmatchedCards: false,
-    cards: [
-        { image: 'abra.png', name: 'abra' },
-        { image: 'aipom.png', name: 'aipom' },
-        { image: 'bulbasaur.png', name: 'bulbasaur' },
-        { image: 'butterfree.png', name: 'butterfree' },
-        { image: 'charmander.png', name: 'charmander' },
-        { image: 'chikorita.png', name: 'chikorita' },
-        { image: 'pikachu.png', name: 'pikachu' },
-        { image: 'dratini.png', name: 'dratini' },
-        { image: 'eevee.png', name: 'eevee' },
-        { image: 'jigglypuff.png', name: 'jigglypuff' },
-        { image: 'mudkip.png', name: 'mudkip' },
-        { image: 'phanpy.png', name: 'phanpy' },
-        { image: 'pichu.png', name: 'pichu' },
-        { image: 'pidgey.png', name: 'pidgey' },
-        { image: 'psyduck.png', name: 'psyduck' },
-        { image: 'seel.png', name: 'seel' },
-        { image: 'slowpoke.png', name: 'slowpoke' },
-        { image: 'squirtle.png', name: 'squirtle' },
-    ],
+    state: {
+        firstCardFlipped: null,
+        secondCardFlipped: null,
+        waitingFlipUnmatchedCards: false,
+        matchedCards: 0,
+    },
+    config: {
+        coverCard: 'pokeball.png',
+        timeToFlipUnmatchedCards: 1 * 1000, // 1 second,
+        cards: [
+            { image: 'abra.png', name: 'abra' },
+            { image: 'aipom.png', name: 'aipom' },
+            { image: 'bulbasaur.png', name: 'bulbasaur' },
+            { image: 'butterfree.png', name: 'butterfree' },
+            { image: 'charmander.png', name: 'charmander' },
+            { image: 'chikorita.png', name: 'chikorita' },
+            { image: 'pikachu.png', name: 'pikachu' },
+            { image: 'dratini.png', name: 'dratini' },
+            { image: 'eevee.png', name: 'eevee' },
+            { image: 'jigglypuff.png', name: 'jigglypuff' },
+            { image: 'mudkip.png', name: 'mudkip' },
+            { image: 'phanpy.png', name: 'phanpy' },
+            { image: 'pichu.png', name: 'pichu' },
+            { image: 'pidgey.png', name: 'pidgey' },
+            { image: 'psyduck.png', name: 'psyduck' },
+            { image: 'seel.png', name: 'seel' },
+            { image: 'slowpoke.png', name: 'slowpoke' },
+            { image: 'squirtle.png', name: 'squirtle' },
+        ],
+        amountOfCardsInGame: 8,
+    },
 
     init: function(element) {
         this.containerElement = element;
-        let amountOfCards = localStorage.getItem('game-amount-cards');
-        if (!amountOfCards) {
-            amountOfCards = 8;
-            localStorage.setItem('game-amount-cards', amountOfCards);
+        
+        let amountOfCardsInLocalStorage = localStorage.getItem('game-amount-cards');
+        if (!!amountOfCardsInLocalStorage && this.config.cards.length < amountOfCardsInLocalStorage) {
+            this.config.amountOfCardsInGame = amountOfCardsInLocalStorage;
+        } else {
+            localStorage.setItem('game-amount-cards', this.config.amountOfCardsInGame);
         }
 
-        if (!!amountOfCards && this.cards.length < amountOfCards) {
-            this.cards = this.cards.slice(0, this.cards.length);
-        }
-            
-        this.cards = this.cards.slice(0, amountOfCards);
+        this.config.cards = this.config.cards.slice(0, this.config.amountOfCardsInGame);
     },
 
     start: function() {
+        this.cleanCardsState();
+        this.state.matchedCards = 0;
         this.startElements();
     },
 
     flipCard: function(element) {
-        if (this.waitingFlipUnmatchedCards) return;
-        if (this.firstCardFlipped === element) return;
+        if (this.state.waitingFlipUnmatchedCards) return;
+        if (this.state.firstCardFlipped === element) return;
 
-        playAudio('assets/audio/card-flip.mp3', VOLUME.LOW);
+        playAudio('assets/audios/card-flip.mp3', VOLUME.LOW);
 
         element.classList.toggle("hidden-card");
 
-        if (!this.firstCardFlipped) {
-            this.firstCardFlipped = element;
+        if (!this.state.firstCardFlipped) {
+            this.state.firstCardFlipped = element;
             return;
         }
 
-        if (!this.secondCardFlipped) {
-            this.secondCardFlipped = element;
+        if (!this.state.secondCardFlipped) {
+            this.state.secondCardFlipped = element;
         }
 
         const isMatchedCards = this.checkMatchCards();
 
-        isMatchedCards ? this.disabledCards() : this.flipUnmatchedCards();
+        isMatchedCards ? this.whenMatchedCards() : this.flipUnmatchedCards();
     },
 
     checkMatchCards: function() {       
-        if (!this.firstCardFlipped && !this.secondCardFlipped) return false;
+        if (!this.state.firstCardFlipped && !this.state.secondCardFlipped) return false;
 
-        return this.firstCardFlipped.dataset.card === this.secondCardFlipped.dataset.card;
+        return this.state.firstCardFlipped.dataset.card === this.state.secondCardFlipped.dataset.card;
+    },
+
+    whenMatchedCards: function() {
+        this.disableMatchedCards();
+        this.cleanCardsState();
+        this.state.matchedCards++;
+
+        if (this.state.matchedCards === this.config.amountOfCardsInGame) {
+            this.doFinalAnimation();
+        } else {
+            this.doAnimationWhenMatchedCards();
+        }
     },
 
     flipUnmatchedCards: function() {
-        this.waitingFlipUnmatchedCards = true;
+        this.state.waitingFlipUnmatchedCards = true;
         setTimeout(() => {
-            this.firstCardFlipped.classList.add('hidden-card');
-            this.secondCardFlipped.classList.add('hidden-card');
+            this.state.firstCardFlipped.classList.add('hidden-card');
+            this.state.secondCardFlipped.classList.add('hidden-card');
             this.cleanCardsState();
-        }, this.timeToFlipUnmatchedCards)
+        }, this.config.timeToFlipUnmatchedCards)
     },
 
-    disabledCards: function() {
-        this.firstCardFlipped.classList.remove('hidden-card');
-        this.secondCardFlipped.classList.remove('hidden-card');
-        this.firstCardFlipped.attributes.removeNamedItem('onclick');
-        this.secondCardFlipped.attributes.removeNamedItem('onclick');
-        document.getElementById('matched-animation').classList.toggle('display');
-        playAudio('assets/audio/pikachu-voice4.wav', VOLUME.HIGH);
+    doFinalAnimation: function() {
+        document.getElementById('final-animation').classList.toggle('display-flex');
+        const audio = new Audio('assets/audios/claps1.mp3');
+        audio.volume = .5;
+        audio.loop = true;
+        audio.play();
 
+        setTimeout(() => {
+            audio.pause();
+            delete audio;
+        }, 90 * 1000) // 90 secs
+    },
+
+    doAnimationWhenMatchedCards: function() {
+        document.getElementById('matched-animation').classList.toggle('display');
+        playAudio('assets/audios/pikachu-voice4.wav', VOLUME.HIGH);
+        
         setTimeout(() => {
             document.getElementById('matched-animation').classList.toggle('display');
         }, 2000);
+    },
 
-        this.cleanCardsState();
+    disableMatchedCards: function() {
+        this.state.firstCardFlipped.classList.remove('hidden-card');
+        this.state.secondCardFlipped.classList.remove('hidden-card');
+        this.state.firstCardFlipped.attributes.removeNamedItem('onclick');
+        this.state.secondCardFlipped.attributes.removeNamedItem('onclick');        
     },
 
     cleanCardsState: function() {
-        [this.firstCardFlipped, this.secondCardFlipped, this.waitingFlipUnmatchedCards] = [null, null, false];
+        [
+            this.state.firstCardFlipped, 
+            this.state.secondCardFlipped, 
+            this.state.waitingFlipUnmatchedCards
+        ] = [null, null, false];
     },
 
     startElements: function() {
         let content = '';
 
-        for (i in this.cards) {
+        for (i in this.config.cards) {
             content += `
                 <div class="card hidden-card" data-card="card-${i}" onClick="memoryGame.flipCard(this)">
                     <div class="card-back-face">
-                        <img src="assets/images/pokemons/${this.cards[i].image}"/>
-                        <p>${this.cards[i].name}</p>
+                        <img src="assets/images/pokemons/${this.config.cards[i].image}"/>
+                        <p>${this.config.cards[i].name}</p>
                     </div>
                     <div class="card-front-face">
                         <p>POKÉMON</p>
-                        <img src="assets/images/${this.coverCard}"/>
+                        <img src="assets/images/${this.config.coverCard}"/>
                     </div>
                 </div>
             `;
